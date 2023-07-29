b784a1619cd2fdce478f1d7711c626bb30f898eb
353718178 <103937491+353718178@users.noreply.github.com>
2023-07-29 11:11:31 +0800

    Delete DFN_Geration directory

--- DFN_Geration/Untitled2.m
+++
@@ -1,7 +0,0 @@
-n=100;
-pts = rand(n,2);                                                        % locations~ U(0,1)
-for i=1:n-1
-    while norm(pts(i,:)-pts(i+1,:)) < 0.01
-        pts(i,:) = rand(1,2);
-    end
-end
--- DFN_Geration/write_dxf_end.m
+++
@@ -1,6 +0,0 @@
-function write_dxf_end(fid)
-fprintf(fid,'  0\n');fprintf(fid,'ENDSEC\n');
-fprintf(fid,'  0\n');fprintf(fid,'EOF\n');
-fclose(fid);
-end
-
